08c6bc9c1a89f2651c32ee5ec3f4e3153b9e019d
HoDinhThe <tauthene@gmail.com>
2024-01-29 10:56:40 +0700

    test

--- src/main/java/com/g7/repository/impl/HoaDonRepository.java
+++ src/main/java/com/g7/repository/impl/HoaDonRepository.java
@@ -55,7 +55,7 @@ public class HoaDonRepository {
                     + "HoaDon.GhiChu, HoaDon.TrangThai, HoaDon.IdNhanVien, HoaDon.IdKhachHang, KhachHang.TenKhachHang, KhachHang.SoDienThoai "
                     + "FROM HoaDon "
                     + "INNER JOIN KhachHang ON HoaDon.IdKhachHang = KhachHang.Id "
-                    + "ORDER BY HoaDon.Id "
+                    + "ORDER BY HoaDon.Id desc "
                     + "OFFSET " + page * 50 + " ROWS FETCH NEXT 50 ROWS ONLY";
 
             PreparedStatement ps = con.prepareStatement(sql);
